17148077d100586626f5515c52d8e6ad961c2c3f
radu-catrangiu <radu.catrangiu@gmail.com>
2019-03-14 22:41:17 +0200

    Move Create Account page script to own file

--- frontend/get_together/src/components/Public/Create/CreateAccount.vue
+++ frontend/get_together/src/components/Public/Create/CreateAccount.vue
@@ -1,34 +1,3 @@
 <template src="./template.html" />
-
-<script>
-/* eslint-disable */
-export default {
-  data() {
-    return {
-      username: "",
-      email: "",
-      password1: "",
-      password2: "",
-      termsAndConditions: false
-    }
-  },
-  methods: {
-    create() {
-      this.$router.push("/");
-    }
-  },
-  mounted() {
-    this.username = this.$route.params.username || "";
-    this.password1 = this.$route.params.password || "";
-    if (this.username.length > 0 || this.password1.length > 0) {
-      this.$refs.email.focus();
-    } else {
-      this.$refs.username.focus();
-    }
-    document.title = "Create Account | Get Together";
-    console.log(this.appConfig);
-  }
-};
-</script>
-
+<script src="./script.js" />
 <style src="./style.css" scoped/>
---
+++ frontend/get_together/src/components/Public/Create/script.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+
+export default {
+    data() {
+        return {
+            username: '',
+            email: '',
+            password1: '',
+            password2: '',
+            termsAndConditions: false
+        };
+    },
+    methods: {
+        create() {
+            createAccount(this);
+        }
+    },
+    mounted() {
+        this.username = this.$route.params.username || '';
+        this.password1 = this.$route.params.password || '';
+        if (this.username.length > 0 || this.password1.length > 0) {
+            this.$refs.email.focus();
+        } else {
+            this.$refs.username.focus();
+        }
+        document.title = 'Create Account | Get Together';
+        console.log(this.appConfig);
+    }
+};
+
+async function createAccount(self) {
+    const params = {
+        username: self.username,
+        email: self.email,
+        password: self.password1
+    };
+    
+    self.$http.callAPI('/core/users', 'create_account', params, (err, res) => {
+        console.log(err, res);
+    });
+}
