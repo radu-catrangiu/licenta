e322cebe787940942905db8bdebbd8244ec65f01
radu-catrangiu <radu.catrangiu@gmail.com>
2019-03-14 22:38:13 +0200

    add simple create_account

--- backend/core_users/src/handler.js
+++ backend/core_users/src/handler.js
@@ -1,16 +1,21 @@
 const uuid = require('uuid');
 
-exports.add = (env, params, done) => {
-    const post = {
+exports.create_account = (env, params, done) => {
+    const account = {
         user_uid: uuid(),
+        info: {},
         ...params
     }
 
-    env.users.insertOne(post, (err, res) => {
+    env.users.insertOne(account, (err, res) => {
         if (err) {
             return done(err);
         }
-        // console.debug(res);
-        done(null, "OK");
+
+        return done(null, "OK");
     });
+};
+
+exports.login = (env, params, done) => {
+    return done(null, "ok");
 }
