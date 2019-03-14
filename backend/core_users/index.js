40649118832bec2d40f38dade5cd1dfc1645d7aa
radu-catrangiu <radu.catrangiu@gmail.com>
2019-03-14 22:37:53 +0200

    switch to core_server_module

--- backend/core_users/index.js
+++ backend/core_users/index.js
@@ -2,7 +2,6 @@ const handler = require('./src/handler');
 const server = require('core_server_module');
 const config = require('./config');
 const modules = require('./src/modules');
-const async = require('async');
 
 const rpc_config = {
     name: "core_users",
@@ -14,21 +13,13 @@ const rpc_config = {
     }
 };
 
-async.waterfall(
-    [
-        modules.load_modules,
-        function(modules, done) {
-            server.init(config.port, rpc_config, modules, error => {
-                if (error) {
-                    console.debug(error);
-                    process.exit(1);
-                }
+server.use('mongo', config.mongo);
+server.init(config.port, rpc_config, modules, error => {
+    if (error) {
+        console.debug(error);
+        process.exit(1);
+    }
 
-                console.log('Server started on port ' + config.port);
-                console.log('http://localhost:' + config.port + '/');
-                done();
-            });
-        }
-    ],
-    (err, res) => console.log(err, res)
-);
+    console.log('Server started on port ' + config.port);
+    console.log('http://localhost:' + config.port + '/');
+});
