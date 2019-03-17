4416cfe2afc5bed622e17d360bac158429141559
radu-catrangiu <radu.catrangiu@gmail.com>
2019-03-17 03:31:07 +0200

    Add buttons in header

--- frontend/get_together/src/components/Dashboard/components/Header.vue
+++ frontend/get_together/src/components/Dashboard/components/Header.vue
@@ -2,15 +2,24 @@
   <div>
     <div class="container">
       <div class="row">
-        <div class="col-sm">
+        <div class="col">
           <img id="logo" src="../../../assets/logo5.svg" alt>
         </div>
-        <div class="col-sm"></div>
-        <div class="col-sm text-right mt-3">
-          <a href="#" v-on:click="show_account_details">
-          <span style="font-size: 5em; color: black;" class="text-right">
-            <i class="fas fa-user-circle"></i>
-          </span>
+        <div class="col text-right mt-4">
+          <a class="mx-4">
+            <span id="groups_button" class="text-right header-btn">
+              <i class="far fa-bell"></i>
+            </span>
+          </a>
+          <a class="mx-4">
+            <span id="groups_button" class="text-right header-btn">
+              <i class="fas fa-users"></i>
+            </span>
+          </a>
+          <a class="ml-4" href="#" v-on:click="show_account_details">
+            <span id="account_button" class="text-right header-btn">
+              <i class="fas fa-user-circle"></i>
+            </span>
           </a>
         </div>
       </div>
@@ -41,5 +50,14 @@ export default {
   /* margin-left: 50%; */
   height: 150px;
   width: 150px;
+}
+
+.header-btn:hover {
+  color: gray;
+}
+
+.header-btn {
+  font-size: 4.5em; 
+  color: black;
 }
 </style>
