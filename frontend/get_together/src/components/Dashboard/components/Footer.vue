f767a6e2371d7b1758428e4666caa575b8889da5
radu-catrangiu <radu.catrangiu@gmail.com>
2019-03-17 03:31:13 +0200

    Add footer

---
+++ frontend/get_together/src/components/Dashboard/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p class="mt-5 mb-3 text-muted text-center">© 2019 Radu Catrangiu.</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
