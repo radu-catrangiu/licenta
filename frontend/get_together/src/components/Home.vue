8128c80b3eb46555ee33eadc463cc999a9478bb0
= <=>
2019-05-02 05:01:19 +0300

    Add validate_token method

--- frontend/get_together/src/components/Home.vue
+++ frontend/get_together/src/components/Home.vue
@@ -4,15 +4,8 @@
 
 <script>
 export default {
-  mounted() {
-    // eslint-disable-next-line
-    console.log(this.$cookie);
-    const user_token = this.$cookie.get('user_token');
-    if (!user_token) {
-      this.$router.push("/login");
-    } else {
-      this.$router.push("/dashboard");
-    }
+  async mounted() {
+    await this.validate_token(this, '/dashboard');
   }
 };
 </script>
---
+++ frontend/get_together/src/utils/validate_token.js
@@ -0,0 +1,26 @@
+export default async function validate_token(self, redirect) {
+    const user_token = self.$cookie.get('user_token');
+    const params = { token: user_token };
+
+    return new Promise ((resolve) => {
+        self.$http.callAPI('/core/tokens', 'validate', params, (err, res) => {
+            // eslint-disable-next-line
+            console.log(err, res);
+            if (err) {
+                self.$router.push('/login');
+                resolve();
+                return;
+            }
+    
+            if (res.status === 0) {
+                if (redirect) {
+                    self.$router.push(redirect);
+                }
+                resolve();
+            } else {
+                self.$router.push('/login');
+                resolve();
+            }
+        });
+    });
+}
