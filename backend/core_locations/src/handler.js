8573a7fa6e7b76e4c68aa75d7a06f00ffcce8309
= <=>
2019-05-03 21:53:07 +0300

    Implement report_location and delete_location

--- backend/core_locations/src/handler.js
+++ backend/core_locations/src/handler.js
@@ -1,3 +1,33 @@
 exports.report_location = (env, params, done) => {
-    done(null, "ok");
+    const group_id = params.group_id;
+    const user_id = params.user_id;
+
+    const query = { group_id, locations: { $elemMatch: { user_id } } };
+    const update = {
+        $set: { [`locations.$.days.${params.day}.lat_lng`]: params.lat_lng }
+    };
+    env.groups.updateOne(query, update, (err, res) => {
+        if (err || !res) {
+            return done(null, { status: 1 });
+        }
+
+        return done(null, { status: 0 });
+    });
+};
+
+exports.delete_location = (env, params, done) => {
+    const group_id = params.group_id;
+    const user_id = params.user_id;
+
+    const query = { group_id, locations: { $elemMatch: { user_id } } };
+    const update = {
+        $set: { [`locations.$.days.${params.day}.lat_lng`]: {} }
+    };
+    env.groups.updateOne(query, update, (err, res) => {
+        if (err || !res) {
+            return done(null, { status: 1 });
+        }
+
+        return done(null, { status: 0 });
+    });
 }
