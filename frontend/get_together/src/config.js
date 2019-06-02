02202108099737d21b8538192a0fb7e81adfe680
= <=>
2019-06-03 00:52:54 +0300

    Add dynamic config to frontend

--- frontend/get_together/src/config.js
+++ frontend/get_together/src/config.js
@@ -2,12 +2,16 @@
 let config;
 
 if (process.env.NODE_ENV === "production") {
+  const API_PORT = process.env.API_PORT || 3333;
+  const API_URL = process.env.API_URL || document.location.hostname;
   config = {
-    $apiUrl: "http://localhost:3333"
+    $apiUrl: `http://${API_URL}:${API_PORT}`
   };
 } else {
+  const API_PORT = process.env.API_PORT || 3333;
+  const API_URL = process.env.API_URL || document.location.hostname;
   config = {
-    $apiUrl: "http://localhost:3333"
+    $apiUrl: `http://${API_URL}:${API_PORT}`
   };
 }
 
