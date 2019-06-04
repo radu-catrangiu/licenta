b98d76e2c828d12eea7e49a276eb1b547398782a
= <=>
2019-06-04 03:24:13 +0300

    Add update_user_info method

--- backend/core_users/src/private_handler.js
+++ backend/core_users/src/private_handler.js
@@ -13,5 +13,44 @@ exports.get_user_info = (env, params, done) => {
 };
 
 exports.update_user_info = (env, params, done) => {
-    
-}
+    const {
+        firstname,
+        lastname,
+        gender,
+        birthdate,
+        profile_picture_id
+    } = params;
+    const user_id = params.user_id;
+    const query = { user_id };
+    const update = {
+        $set: {}
+    };
+
+    if (typeof firstname === 'string' && firstname.length > 0) {
+        update.$set['info.firstname'] = firstname;
+    }
+
+    if (typeof lastname === 'string' && lastname.length > 0) {
+        update.$set['info.lastname'] = lastname;
+    }
+
+    if (typeof gender === 'string' && gender.length > 0) {
+        update.$set['info.gender'] = gender;
+    }
+
+    if (typeof birthdate === 'string' && birthdate.length > 0) {
+        update.$set['info.birthdate'] = new Date(birthdate);
+    }
+
+    if (typeof profile_picture_id === 'string' && profile_picture_id.length > 0) {
+        update.$set['info.profile_picture_id'] = profile_picture_id;
+    }
+
+    env.users.updateOne(query, update, (err, res) => {
+        if (err || !res) {
+            return done('Something went wrong creating the group');
+        }
+
+        return done(null, { status: 0 });
+    });
+};
