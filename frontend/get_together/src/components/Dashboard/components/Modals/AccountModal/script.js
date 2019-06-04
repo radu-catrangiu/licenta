a7300dc6ee90ace561030507a1b03013b601f42b
= <=>
2019-06-04 03:25:10 +0300

    Update account details modal

--- frontend/get_together/src/components/Dashboard/components/Modals/AccountModal/script.js
+++ frontend/get_together/src/components/Dashboard/components/Modals/AccountModal/script.js
@@ -3,8 +3,8 @@ import 'pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css';
 import {VueCroppieComponent} from 'vue-croppie';
 
 let VueCroppie = VueCroppieComponent;
-let date = new Date();
-date.setFullYear(date.getFullYear() - 13);
+let max_date = new Date();
+// date.setFullYear(date.getFullYear() - 13);
 
 export default {
     components: {
@@ -15,11 +15,12 @@ export default {
         return {
             firstname: '',
             lastname: '',
-            date: new Date(),
+            gender: 'unspecified',
+            birthdate: "",
             options: {
                 format: 'DD.MM.YYYY',
                 minDate: new Date('1900-01-01T00:00:00.000Z'),
-                maxDate: date,
+                maxDate: max_date,
                 icons: {
                     time: 'far fa-clock',
                     date: 'far fa-calendar',
@@ -34,10 +35,29 @@ export default {
             },
             filename: "",
             image: null,
-            cropped: null
+            cropped: null,
+            profile_picture_id: "",
+            old_profile_picture_id: ""
         };
     },
+    computed: {
+        profile_pic_url() {
+            const api_url = this.appConfig.$apiUrl;
+            return `${api_url}/profile_picture/${this.old_profile_picture_id}.png`
+        }
+    },
     mounted() {
+        this.$store.subscribe((mutation) => {
+            if (mutation.type === 'set_user_info') {
+                const { info } = this.$store.getters.user_info;
+                this.firstname = info.firstname || "";
+                this.lastname = info.lastname || "";
+                this.gender = info.gender || "unspecified";
+                this.old_profile_picture_id = info.profile_picture_id || "";
+                this.birthdate = info.birthdate ? new Date(info.birthdate) : this.birthdate;
+            }
+        });
+
         /* eslint-disable */
         this.$('#accountModal').on('hidden.bs.modal', e => {
             this.$('div#dashboard').css('-webkit-filter', 'blur(0px)');
@@ -48,6 +68,28 @@ export default {
         });
     },
     methods: {
+        async update() {
+            if (this.image != null) {
+                this.crop(() => {
+                    upload_file(this, () => {
+                        update_user_info(this, (err, res) => {
+                            console.log(err, res);
+                            this.$('#accountModal').modal('hide');
+                            this.image = "";
+                            this.cropped = "";
+                            this.filename = "";
+                            this.old_profile_picture_id = this.profile_picture_id;
+                        });
+                    });
+                });
+            } else {
+                update_user_info(this, (err, res) => {
+                    console.log(err, res);
+                    this.$('#accountModal').modal('hide');
+                });
+            }
+
+        },
         logout() {
             this.$cookie.delete('group_id');
             this.$cookie.delete('user_token');
@@ -78,13 +120,15 @@ export default {
                 url: url,
             });
         },
-        crop() {
+        crop(callback) {
             let options = {
+                type: 'blob',
                 format: 'png', 
                 circle: true
             }
             this.$refs.croppieRef.result(options, (output) => {
                 this.cropped = output;
+                callback();
             });
         },
         rotate(rotationAngle) {
@@ -93,3 +137,44 @@ export default {
         }
     }
 };
+
+function update_user_info(self, done) {
+    const user_token = self.$cookie.get("user_token");
+    let from, birthdate;
+    if (self.birthdate) {
+        from = self.birthdate.split('.');
+        birthdate = new Date(from[2], from[1] - 1, from[0])
+    }
+    const params = {
+        user_token,
+        firstname: self.firstname,
+        lastname: self.lastname,
+        gender: self.gender,
+        birthdate: birthdate || undefined,
+        profile_picture_id: self.profile_picture_id
+    };
+
+    console.log(JSON.stringify(params));
+    self.$http.callAPI('/core/user_mgmt', 'update_user_info', params, (err, res) => {
+        if (err) {
+            return done(err);
+        }
+
+        return done(null, res);
+    });
+}
+
+async function upload_file(self, callback) {
+    const user_token = self.$cookie.get('user_token');
+    const api_url = self.appConfig.$apiUrl;
+    const data = new FormData();
+
+    data.append('user_token', user_token);
+    data.append('file', self.cropped);
+    console.log(self.cropped);
+
+    let res = await self.$http.post(`${api_url}/upload/picture`, data);
+    self.profile_picture_id = res.data.picture_id;
+    console.log(res)
+    callback();
+}
