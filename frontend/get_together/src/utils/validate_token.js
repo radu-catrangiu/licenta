5151df74d630d3521237b083134827331271af0d
radu-catrangiu <radu.catrangiu@gmail.com>
2020-01-05 18:38:43 +0200

    Remove cookie when token invalid

--- frontend/get_together/src/utils/validate_token.js
+++ frontend/get_together/src/utils/validate_token.js
@@ -1,10 +1,16 @@
 export default async function validate_token(self, redirect) {
     const user_token = self.$cookie.get('user_token');
+    if (!user_token) {
+        self.$cookie.remove('user_token');
+        self.$router.push('/login');
+        return;
+    }
     const params = { token: user_token };
 
     return new Promise ((resolve) => {
         self.$http.callAPI('/core/tokens', 'validate', params, (err, res) => {
             if (err) {
+                self.$cookie.remove('user_token');
                 self.$router.push('/login');
                 resolve();
                 return;
@@ -16,6 +22,7 @@ export default async function validate_token(self, redirect) {
                 }
                 resolve();
             } else {
+                self.$cookie.remove('user_token');
                 self.$router.push('/login');
                 resolve();
             }
